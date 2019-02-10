9b41596f40b0afdac0f14c660f365c14536ff78a
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-02-10 05:02:49 -0700

    changes done for coding practise

---
+++ src/asu/edu/Arav/ImpG/ProductReviewScore.java
@@ -0,0 +1,11 @@
+package asu.edu.Arav.ImpG;
+
+public class ProductReviewScore {
+	
+	public int productId;
+	public double reviewScore;
+	public ProductReviewScore(int id,  double value) {
+		this.productId = id;
+		this.reviewScore = value;
+	}
+}
--- src/asu/edu/AravL/algo/Sorts.java
+++ src/asu/edu/AravL/algo/Sorts.java
@@ -27,55 +27,54 @@ public class Sorts {
 	}
 
 	public void merge(int[] a, int f, int m, int l) {
-		
-		/*100 million elements were sorted in 13 secs
-		 	10 million elements in 1.5 secs
-		 	1 million elements in .2 secs
-		 	100k elements in 0.027 secs
-		 	200k elements in 0.05 secs
-		 */		
-		int l1 = m - f + 1; // from first index to mid index  +1 compensating for the length lost in the previous step
-		int r1 = l - m; // from the mid index to the last one 
-		
-		int [] l_1 = new int[l1];
-		int [] r_1 = new int[r1];
-		
-		for(int i = 0; i < l1; i++) {
-			l_1[i] = a[f + i];// filling the left subarray remember the index 'f'  till the middle element
+
+		/*
+		 * 100 million elements were sorted in 13 secs 10 million elements in 1.5 secs 1
+		 * million elements in .2 secs 100k elements in 0.027 secs 200k elements in 0.05
+		 * secs
+		 */
+		int l1 = m - f + 1; // from first index to mid index +1 compensating for the length lost in the
+							// previous step
+		int r1 = l - m; // from the mid index to the last one
+
+		int[] l_1 = new int[l1];
+		int[] r_1 = new int[r1];
+
+		for (int i = 0; i < l1; i++) {
+			l_1[i] = a[f + i];// filling the left subarray remember the index 'f' till the middle element
 		}
-		
-		for(int i = 0; i < r1; i++) {
-			r_1[i] = a[m + i +1];// filling the left subarray   also 'm' wont be included in this subarray 
+
+		for (int i = 0; i < r1; i++) {
+			r_1[i] = a[m + i + 1];// filling the left subarray also 'm' wont be included in this subarray
 		}
-		
-		
-		int i =0, j = 0;
+
+		int i = 0, j = 0;
 		// we will be merging the two sorted subarrays now
-		while(i < l1 && j <r1) {
-			if(l_1[i] < r_1[j]) {
+		while (i < l1 && j < r1) {
+			if (l_1[i] < r_1[j]) {
 				a[f] = l_1[i];
 				i++;
-			}else {
+			} else {
 				a[f] = r_1[j];
 				j++;
 			}
 			f++;
 		}
-		
-		// if one sub array becomes empty fill the resultant with the rest of elements of the other array
-		while(i< l1) {
+
+		// if one sub array becomes empty fill the resultant with the rest of elements
+		// of the other array
+		while (i < l1) {
 			a[f] = l_1[i];
 			f++;
 			i++;
 		}
-		
-		while(j< r1) {
+
+		while (j < r1) {
 			a[f] = r_1[j];
 			f++;
 			j++;
 		}
-		
-		
+
 	}
 
 	public void mergeSort(int[] a, int f, int l) {
@@ -87,9 +86,38 @@ public class Sorts {
 		}
 	}
 
+	public int partition(int[] a, int p, int r) {
+		int pivot = a[r]; // the element which we choose to make it the pivot.
+		int i = p - 1; // the indexes till which all the elements are lesser than the pivot
+		for (int j = p; j < r; j++) { // for elements to segregate would start from the first element but keep in
+										// mind it wont include the last element ?? As thats the pivot u see // r-1 wont
+										// be right
+			if (a[j] <= pivot) {
+				i++;
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+		int temp = a[r];
+		a[r] = a[i + 1];
+		a[i + 1] = temp;
+		return i + 1;
+	}
+
+	public void quickSort(int[] a, int p, int r) {
+		// Performance is comparable to merge sort, but the performance degrades
+		// drastically if the pivoting is bad i.e stack overflow conditions will occur
+		if (p < r) {
+			int q = partition(a, p, r);
+			quickSort(a, p, q - 1);
+			quickSort(a, q + 1, r);
+		}
+	}
+
 	public static void main(String[] args) {
 		Random rn = new Random();
-		int[] input = new int[200000];
+		int[] input = new int[100000000];
 
 		for (int i = 0; i < input.length; i++) {
 			input[i] = rn.nextInt(10 - 1 + 1) + 1;
@@ -98,27 +126,42 @@ public class Sorts {
 		Sorts s = new Sorts();
 
 		long startTime = System.nanoTime();
-		//input = s.insertionSort(input);
+		// input = s.insertionSort(input);
 		long endTime = System.nanoTime();
 		long duration = (endTime - startTime);
-		
+
 		System.out.println("Time taken for insertion  sort " + duration / 1000000);
-		
+
 		for (int i = 0; i < input.length; i++) {
 			input[i] = rn.nextInt(10 - 1 + 1) + 1;
 		}
-		
+
 		startTime = System.nanoTime();
-		s.mergeSort(input, 0, input.length-1);
+		s.mergeSort(input, 0, input.length - 1);
 		endTime = System.nanoTime();
 		duration = (endTime - startTime);
 
-		/*for(int i = 0;  i < input.length; i ++) {
-			System.out.println(input[i] + "\t");
-		}*/
+		/*
+		 * for(int i = 0; i < input.length; i ++) { System.out.println(input[i] + "\t");
+		 * }
+		 */
 
 		System.out.println("Time taken for Merge sort " + duration / 1000000);
 
+		for (int i = 0; i < input.length; i++) {
+			input[i] = rn.nextInt(10000000 - 1 + 1) + 1;
+		}
+
+		startTime = System.nanoTime();
+		s.quickSort(input, 0, input.length - 1);
+		endTime = System.nanoTime();
+		duration = (endTime - startTime);
+
+		/*
+		 * for(int i = 0; i < input.length; i ++) { System.out.println(input[i] + "\t");
+		 * }
+		 */
+		System.out.println("Time taken for Quick sort " + duration / 1000000);
 	}
 
 }
