61b6762db1f594ebc513ecefce8609c122f52a7f
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-02-11 03:14:12 -0700

    changes for today 10 feb 2019

---
+++ src/asu/edu/Arav/ImpG/IslandPerimeter.java
@@ -0,0 +1,33 @@
+package asu.edu.Arav.ImpG;
+
+public class IslandPerimeter {
+	public int islandPerimeter(int[][] grid) {
+		int p = 0;
+		for(int i = 0; i < grid.length; i++) {
+			for(int j=0; j < grid[0].length; j++) {
+				if(grid[i][j] == 1) {
+					if(i == 0 || grid[i-1][j] == 0) {
+						p++;
+					}
+					if(i == grid.length-1 || grid[i+1][j] == 0) {
+						p++;
+					}
+					if(j == 0 || grid[i][j-1] == 0) {
+						p++;
+					}
+					if(j == grid[0].length-1 || grid[i][j+1] == 0) {
+						p++;
+					}
+				}
+			}
+		}
+		
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
