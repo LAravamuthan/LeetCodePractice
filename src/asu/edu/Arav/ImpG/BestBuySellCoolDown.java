4c90195dad7947b19886ee13d37fdcadde48190c
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-05-13 01:26:50 -0700

    Changes done as part of 13 May 2019

--- src/asu/edu/Arav/ImpG/BestBuySellCoolDown.java
+++ src/asu/edu/Arav/ImpG/BestBuySellCoolDown.java
@@ -4,6 +4,7 @@ package asu.edu.Arav.ImpG;
  * @author Aravamuthan Lakshminarayanan
  * @project LeetCodePractice
  */
+@SuppressWarnings("Duplicates")
 public class BestBuySellCoolDown {
 
 
@@ -11,11 +12,45 @@ public class BestBuySellCoolDown {
         int[] buy = new int[prices.length];
         int[] sell = new int[prices.length];
 
-        for(int i = 0; i < prices.length; i++){
-            
+        if (prices == null || prices.length == 0) {
+            return 0;
         }
 
-        return 0;
+        buy[0] = -prices[0];
+        sell[0] = 0;
+
+
+        for (int i = 1; i < prices.length; i++) {
+            if (i - 2 < 0) {
+                buy[i] = Math.max(buy[i - 1], 0 - prices[i]);
+            } else {
+                buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
+            }
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+        }
+        return sell[prices.length - 1];
+    }
+
+    public int maxProfit1(int[] prices) {
+
+
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int b1 = -prices[0];
+        int s1 = 0;
+        int s2 = 0;
+
+
+        for (int i = 1; i < prices.length; i++) {
+            int b0 = Math.max(b1, s2 - prices[i]);
+            int s0 = Math.max(s1, b1 + prices[i]);
+            s2 = s1;
+            s1 = s0;
+            b1 = b0;
+        }
+        return s1;
     }
 
 }
