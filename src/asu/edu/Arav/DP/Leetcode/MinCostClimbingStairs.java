5e9da5e43ffec6b815a30533b3a84ad38bbd0081
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-05-22 00:53:42 -0700

    2 DP and 1 Bhav

---
+++ src/asu/edu/Arav/DP/Leetcode/MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+package asu.edu.Arav.DP.Leetcode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @project LeetCodePractice
+ */
+public class MinCostClimbingStairs {
+
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost == null || cost.length == 0) {
+            return 0;
+        }
+        int[] result = new int[cost.length];
+        result[0] = cost[0];
+        result[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            result[i] = Math.min(cost[i] + result[i - 1], cost[i] + result[i - 2]);
+        }
+        return Math.min(result[cost.length - 1], result[cost.length - 2]);
+    }
+}
