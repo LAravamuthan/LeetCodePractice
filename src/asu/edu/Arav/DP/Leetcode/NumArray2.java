7aa99db47f10cf6cac1a04c93e205c2b6cadff94
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-05-27 09:07:21 -0700

    changes till 27 May 2019

---
+++ src/asu/edu/Arav/DP/Leetcode/NumArray2.java
@@ -0,0 +1,27 @@
+package asu.edu.Arav.DP.Leetcode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @project LeetCodePractice
+ */
+
+public class NumArray2 {
+
+    public int[] sums;
+
+    public NumArray2(int[] nums) {
+        this.sums = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                this.sums[i] = nums[i];
+            } else {
+                this.sums[i] = this.sums[i - 1] + nums[i];
+            }
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return i == 0 ? this.sums[j] : this.sums[j] - this.sums[i - 1];
+    }
+}
+
---
+++ src/asu/edu/Arav/DP/Leetcode/StoneGame.java
@@ -0,0 +1,31 @@
+package asu.edu.Arav.DP.Leetcode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @project LeetCodePractice
+ */
+
+public class StoneGame {
+    public static boolean stoneGame(int[] piles) {
+        int N = piles.length;
+
+        // dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]].
+        int[][] dp = new int[N + 2][N + 2];
+        for (int size = 1; size <= N; ++size)
+            for (int i = 0; i + size <= N; ++i) {
+                int j = i + size - 1;
+                int parity = (j + i + N) % 2;  // j - i - N; but +x = -x (mod 2)
+                if (parity == 1)
+                    dp[i + 1][j + 1] = Math.max(piles[i] + dp[i + 2][j + 1], piles[j] + dp[i + 1][j]);
+                else
+                    dp[i + 1][j + 1] = Math.min(-piles[i] + dp[i + 2][j + 1], -piles[j] + dp[i + 1][j]);
+            }
+
+        return dp[1][N] > 0;
+    }
+
+    public static void main(String[] args) {
+        int [] input = {3,2,10,4};
+        System.out.println(stoneGame(input));
+    }
+}
