e8b0404a4e37238e0125ad0d63623bfd6ea50079
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-06-03 07:12:50 -0700

    Commits to be done

---
+++ src/asu/edu/Arav/DP/Leetcode/Two2KeyBoard.java
@@ -0,0 +1,19 @@
+package asu.edu.Arav.DP.Leetcode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @project LeetCodePractice
+ */
+public class Two2KeyBoard {
+    public int minSteps(int n) {
+        int ans = 0; int d =2;
+        while(n > 1){
+            while(n %d  == 0){
+                ans+= d;
+                n/=d;
+            }
+            d++;
+        }
+        return ans;
+    }
+}
