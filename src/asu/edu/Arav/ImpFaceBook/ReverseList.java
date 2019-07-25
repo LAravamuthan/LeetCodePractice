a1457fe6b7fba4a1e4efe3d28c8b8fbee1ff731f
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-07-24 20:38:48 -0700

    FaceBook questions

---
+++ src/asu/edu/Arav/ImpFaceBook/ReverseList.java
@@ -0,0 +1,62 @@
+package asu.edu.Arav.ImpFaceBook;
+
+import asu.edu.AravL.utility.ListNode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @author Bhavana Vakkalagadda
+ * @project LeetCodePractice
+ */
+public class ReverseList {
+
+    public ListNode head;
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        recursive(head);
+        head.next = null;
+        return this.head;
+
+    }
+
+    public void recursive(ListNode node){
+        if(node.next == null){
+            this.head = node;
+            return;
+        }
+        recursive(node.next);
+        node.next.next = node;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public ListNode reverseList3(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = head.next;
+
+        while(next!=null){
+            current.next = previous;
+            previous = current;
+            current = next;
+            next = next.next;
+        }
+
+        head = current;
+        return head;
+    }
+
+}
