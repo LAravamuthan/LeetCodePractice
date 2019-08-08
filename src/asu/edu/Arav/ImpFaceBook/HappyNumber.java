ee955a4727ceb06661098d87a76eb413501b8403
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-08-07 18:42:02 -0700

    Easy Questions

---
+++ src/asu/edu/Arav/ImpFaceBook/HappyNumber.java
@@ -0,0 +1,42 @@
+package asu.edu.Arav.ImpFaceBook;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+
+    public static int[] digitsArray = new int[11];
+
+    public static boolean isHappy(int n) {
+        Set<Integer> foundSums = new HashSet<Integer>();
+        int sum = n;
+        int indexOfDigitsArray = 0;
+        while (!foundSums.contains(sum)) {
+            foundSums.add(sum);
+            indexOfDigitsArray = getDigitsArray(sum);
+            sum = 0;
+            for (int i = 0; i < indexOfDigitsArray; i++) {
+                sum += digitsArray[i] * digitsArray[i];
+            }
+            if (sum == 1) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static int getDigitsArray(int n) {
+        int index = 0;
+        while (n != 0) {
+            digitsArray[index] = n % 10;
+            n /= 10;
+            index++;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+}
