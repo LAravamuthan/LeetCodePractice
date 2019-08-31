bb69c08618b2bb20210f3e69ac65b6a6575c158c
Aravamuthan Lakshminarayanan (Student) <alaksh17@sundevils.asu.edu>
2019-08-31 03:48:24 -0700

    balanced binary Tree problem

---
+++ src/asu/edu/AravL/easy/BalancedBinaryTree.java
@@ -0,0 +1,50 @@
+package asu.edu.AravL.easy;
+
+import asu.edu.AravL.utility.TreeNode;
+
+public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftTreeHeight = 0;
+        int rightTreeHeight = 0;
+        boolean isLeftTreeFine = true;
+        boolean isrightTreeFine = true;
+
+        if (root.left != null) {
+            isLeftTreeFine = isBalanced(root.left);
+
+        }
+        if(isLeftTreeFine){
+            leftTreeHeight = maxDepth(root.left);
+        }else{
+            return false;
+        }
+        if (root.right != null) {
+            isrightTreeFine = isBalanced(root.right);
+        }
+        if(isrightTreeFine){
+            rightTreeHeight = maxDepth(root.right);
+        }else{
+            return false;
+        }
+        return Math.abs(leftTreeHeight - rightTreeHeight) < 2 ? true : false;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftTreeHeight = 0;
+        int rightTreeHeight = 0;
+        if (root.left != null) {
+            leftTreeHeight = maxDepth(root.left);
+        }
+        if (root.right != null) {
+            rightTreeHeight = maxDepth(root.right);
+        }
+        return 1 + Math.max(leftTreeHeight, rightTreeHeight);
+    }
+}
