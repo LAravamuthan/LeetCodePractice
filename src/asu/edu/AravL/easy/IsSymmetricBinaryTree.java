7aaeef7ef45c12291bc7e1028fdff69cae8eb4b4
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-09-02 00:22:32 -0700

    IsSymmetricBinaryTree

--- src/asu/edu/AravL/easy/IsSymmetricBinaryTree.java
+++ src/asu/edu/AravL/easy/IsSymmetricBinaryTree.java
@@ -9,7 +9,19 @@ import asu.edu.AravL.utility.TreeNode;
  */
 public class IsSymmetricBinaryTree {
     public boolean isSymmetric(TreeNode root) {
+        return isTreeMirror(root, root);
+    }
 
-        return false;
+    public boolean isTreeMirror(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isTreeMirror(s.left, t.right) && isTreeMirror(s.right, t.left);
     }
 }
