57f8e3c1ba332c0843959f9f7c459e438b8c05da
Aravamuthan Lakshminarayanan <l.aravamuthan@gmail.com>
2019-09-02 00:14:51 -0700

    Some Easy problems

---
+++ src/asu/edu/AravL/easy/IsSymmetricBinaryTree.java
@@ -0,0 +1,15 @@
+package asu.edu.AravL.easy;
+
+import asu.edu.AravL.utility.TreeNode;
+
+/**
+ * @author Aravamuthan Lakshminarayanan
+ * @author Bhavana Vakkalagadda
+ * @project LeetCodePractice
+ */
+public class IsSymmetricBinaryTree {
+    public boolean isSymmetric(TreeNode root) {
+
+        return false;
+    }
+}
