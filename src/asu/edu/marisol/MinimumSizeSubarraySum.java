2a120ee5b624cbac1a1f7abaca976b5aab0c1ddd
LAravamuthan <l.aravamuthan@gmail.com>
2025-06-06 13:27:33 -0700

    Jun-6-2025:13:27hrs

--- src/asu/edu/marisol/MinimumSizeSubarraySum.java
+++ src/asu/edu/marisol/MinimumSizeSubarraySum.java
@@ -51,7 +51,7 @@ public class MinimumSizeSubarraySum {
     }
 
     /**
-     * Sliding window approach.
+     * Sliding window approach, Prefix sum approach.
      *
      * @time-complexity - O(n).
      * @space-complexity - O(1).
@@ -70,6 +70,74 @@ public class MinimumSizeSubarraySum {
         }
 
         return res == nums.length + 1 ? 0 : res;
+    }
+
+
+    /**
+     * Sliding window approach, Prefix sum approach.
+     * Practice.
+     *
+     * @time-complexity - O(n).
+     * @space-complexity - O(1).
+     */
+    public int minSubArrayLen3(int target, int[] nums) {
+        int res = nums.length + 1;
+        int low = 0, high = 0, curSum = 0;
+
+        while (high < nums.length) {
+            curSum += nums[high];
+
+            while (curSum >= target && low <= high) {
+                res = Math.min(res, high - low + 1);
+                curSum -= nums[low++];
+            }
+            high++;
+        }
+
+        return res == nums.length + 1 ? 0 : res;
+    }
+
+
+    /**
+     * Binary Search approach, Practice.
+     *
+     * Imp: The thing to note here is the fact that, working in indices with lo = 0 and hi = length - 1 , won't work at all.
+     * The problem here is not the boundary management, but the context itself is different. Basically the length only value
+     * we need to work with.
+     *
+     * @time-complexity - O(nlogn).
+     * @space-complexity - O(1).
+     */
+    public int minSubArrayLen4(int target, int[] nums) {
+        int lo = 1, hi = nums.length;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (lengthMakesTarget(nums, target, mid))
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+
+        return lengthMakesTarget(nums, target, lo) ? lo : 0;
+    }
+
+    private boolean lengthMakesTarget(int[] nums, int target, int length) {
+        int i = 0, curSum = 0, n = nums.length;
+
+        for (; i < length; i++) {
+            curSum += nums[i];
+            if (curSum >= target) return true;
+        }
+
+        while (i < n) {
+            curSum += nums[i];
+            curSum -= nums[i - length];
+            if (curSum >= target) return true;
+            i++;
+        }
+
+        return false;
     }
 
 }
